aca4329147c9ef1264b29e3e6b79ddf1419de56f
dante <p.k.golabek@gmail.com>
2019-07-18 13:10:11 +0200

    modified:   lspo/SoundProcessor.py 	modified:   test_script.py

--- lspo/SoundProcessor.py
+++ lspo/SoundProcessor.py
@@ -12,12 +12,39 @@ class sopro(object):
 		fss = np.zeros(len(sounds))
 		for sound_number in range(len(sounds)):
 			signals[sound_number,:], fss[sound_number] = self.read_song(sounds[sound_number])
-		self.shape = np.shape(signals)
-		self.N = self.shape[-1]
+		self._signals = signals
+		self._shape = np.shape(signals)
+		self._N = self._shape[-1]
 		self.fs = fss[0]
-		self.t = self.N/self.fs
-		self.T = np.linspace(0,self.t,self.N)
-		self.signals = signals
+		self._t = self.N/self.fs
+		self._T = np.linspace(0,self.t,self.N)
+		self._vocal = signals[0]
+		self._music = signals[1]
+
+	@property
+	def signals(self):
+		return self._signals
+	@property
+	def vocal(self):
+		return self._signals[0]
+	@property
+	def music(self):
+		return self.signals[1]
+	@property
+	def N(self):
+		return len(self._signals[0])
+	@property
+	def shape(self):
+		return np.shape(self.signals)
+	@property
+	def t(self):
+		return self.N/self.fs
+	@property
+	def T(self):
+		return np.linspace(0,self.t,self.N)
+	
+	
+	
 
 	def read_song(self,sound):
 		with open(sound,'rb') as file1:
@@ -29,19 +56,26 @@ class sopro(object):
 		of full song.'''
 		pass
 
-	def cut_border_silence(self):
-		pass
-
+	def cut_border_silence(self,window_len=1000):
+		tresh = np.max(np.abs(self.vocal))*0.0003
+		step = int(np.floor(len(self.vocal)/window_len))
+		i=0
+		while np.sum(np.abs(self.vocal[step*i:step*(i+1)]))<tresh:
+			i+=1
+		i=1
+		while np.sum(np.abs(self.vocal[step*i*-1-1:step*i*-1]))<tresh:
+			i+=1
+		self._signals = np.dstack([self._signals[0,:step*i*-1],self._signals[1,:step*i*-1]]) 
 
 	def windows_power(self,window_len=0.5,noverlap=0.25):
 		'''divides vocal into windows, calculates their overall power, returns
-		that as a list and indexes of middles of those windows.'''
+		that as a list, size of a step and indexes of middles of those windows.'''
 		window_len, noverlap = window_len*self.fs, noverlap*self.fs
 		step = int(np.floor(window_len-noverlap))
 		sums = np.zeros(int(len(np.floor(self.signals[0])/(step))))
 		for i in range(len(sums)):
 			sums[i] = np.sum(np.abs(self.signals[0,i*(step):(i+1)*(step)]))
-		return sums, step, range(1,len(sums))*step
+		return sums, step, np.arange(1,len(sums))*step
 
 	def min_window(self,window_len=0.5,noverlap=0.25):
 		sums, step, middles = self.windows_power(window_len=0.5,noverlap=0.25)
--- test_script.py
+++ test_script.py
@@ -1,10 +1,19 @@
 from lspo.SoundProcessor import sopro
 from lspo import withNoName
 import matplotlib.pyplot as plt 
+import os.path
 
 
-sample = sopro(['chandelier.wav','test.wav'])
-print (sample.N, sample.t,sample.shape)
-plt.plot(sample.T,sample.signals[0,:])
-plt.axvline(sample.min_window(2,1)/sample.fs)
+
+path = os.getcwd()
+parent = os.path.abspath(os.path.join(path, os.pardir))
+sample = sopro([parent + '/chandelier.wav',parent + '/test.wav'])
+print ('number of samples is: {}\ntime in seconds is: {}\nand shape: {}'
+	.format(sample.N, sample.t,sample.shape))
+#plt.plot(sample.T[-120000:-1],sample.vocal[-120000:-1],alpha=1)
+sample.cut_border_silence()
+plt.plot(sample.T,sample.vocal,alpha=0.6)
+print ('\n\nnumber of samples is: {}\ntime in seconds is: {}\nand shape: {}'
+	.format(sample.N, sample.t,sample.shape))
+#plt.axvline(sample.min_window(2,1)/sample.fs)
 plt.show()
